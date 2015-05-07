d0a5f7670ea25f11edf35e949756edc4a6651f5f
Claudia <mccc86@gmail.com>
2015-05-06 21:30:01 -0700

    added readme file and comments on script.js

---
+++ README.md
@@ -0,0 +1,19 @@
+First, I started looking at google maps api and example code snippets on how to create simple map with markers, etc. 
+
+Then, I started by checking knockoutjs documentation and finding examples that fit this project.
+
+I found a code example that binds the search box with the list view, using knockoutjs.
+
+After all of the above, I started puttting this pieces together to create the map.
+
+Also, I used foursquare api and jQuery ajax calls to request places around an specific location. 
+
+
+Resources:
+https://developers.google.com/maps/documentation/javascript/examples/map-simple
+http://jsfiddle.net/mythical/XJEzc/ 
+http://knockoutjs.com/documentation/textinput-binding.html
+https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+http://stackoverflow.com/questions/4269419/dynamic-google-map-infowindow-loading-content-of-last-record-in-loop
+https://developer.foursquare.com/docs/venues/venues
+http://api.jquery.com/jquery.getjson/
--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
+//setting up default variables
 var map;
 var  placesInEncino = [];
 
+//This function is in charge of setting up KO search box binding and filtering the array of
+//places based user input.
 var setupKO = function() {
+	
+	//KO view model definition
 	var viewModel = {
+		//search box text binding
 		query: ko.observable(''),
+		
+		//mouseOver event handler
 		logMouseOver: function(place) {
 			google.maps.event.trigger(place.marker, 'click');
 		}
 	};
-
+	
+	
+	//KO placesInEncino array binding. Everytime the a user is typing in
+	//the search box, this obserbable function is call and filter the
+	//places by its name and depening on user input.
 	viewModel.placesInEncino = ko.dependentObservable(function() {
+		
+		//get user input (needle)
 		var search = this.query().toLowerCase();
+		
+		//hides all markers on the map
 		setAllMarkers(null);
+		
+		
 		return ko.utils.arrayFilter(placesInEncino, function(place) {
 			
+			//find the needle in the haystack (use the native indexOf on the place name)
 			if(place.name.toLowerCase().indexOf(search) >= 0) {
+				//display the marker to those found places
 				place.marker.setMap(map);
 				return true;
 			}
@@ -26,17 +46,20 @@ var setupKO = function() {
 	ko.applyBindings(viewModel);
 }
 
+//utility function in which hides/show the markers on the map
 var setAllMarkers = function(map) {
 	
 	for (var i = 0; i < placesInEncino.length; i++) {
 		var place = placesInEncino[i];
 		place.marker.setMap(map);
-		
 	}
 
 }
 
+//bootstrap the places and makers on the map
 var displayPlaces = function(map) {
+	
+	//iterate over the places array and creates a marker and infowindow object on a click event.
 	for (var i = 0; i < placesInEncino.length; i++) {
 		
 		var place = placesInEncino[i];
@@ -46,8 +69,12 @@ var displayPlaces = function(map) {
 			animation: google.maps.Animation.DROP,
 			position: new google.maps.LatLng(place.location.lat, place.location.lng)
 		});
+		
+		//build infowindow content
 		var contentString = '<strong><a href="https://foursquare.com/v/' + place.id + '">' + place.name + '</a></strong>' + '<p>' + place.location.city + ', ' + place.location.state + '</p>';
 		createInfoWindow(marker, contentString);
+		
+		//keeps a pointer for later use.
 		place.marker = marker;	
 
 	}
@@ -62,15 +89,21 @@ var displayPlaces = function(map) {
 	}
 }
 
+
+//Performs a Foursqure Api request
 function getFoursquareData(callback) {
 	
+	var clientId = 'PBXSACZTMPAFLIMEURWTEI4HRRONHERVT21KHLJQGUFRZU5M';
+	var clientSecret = 'AUUZ1ZUCFSYTXQHJCPMVBEKBTWA2AJO55AARMUUP3ZUD3C3O';
+	
 	var url =  'https://api.foursquare.com/v2/venues/search' +
-	'?ll=34.16,-118.52&client_id=PBXSACZTMPAFLIMEURWTEI4HRRONHERVT21KHLJQGUFRZU5M' +
-	'&client_secret=AUUZ1ZUCFSYTXQHJCPMVBEKBTWA2AJO55AARMUUP3ZUD3C3O&v=20150428';
+	'?ll=34.16,-118.52&client_id=' + clientId + 
+	'&client_secret=' + clientSecret + '&v=20150428';
 
 	$.getJSON( url, callback);
 }
 
+//main initalization function for google maps and the project in general.
 function initialize() {
 	var Encino = new google.maps.LatLng(34.163593, -118.521489);
 	
@@ -81,11 +114,12 @@ function initialize() {
 	
 	
 	var callback = function(data) {
-		placesInEncino = data.response.venues.slice(0,15);
+		placesInEncino = data.response.venues.slice(0,10);
 		displayPlaces(map);
 		setupKO();
 	};
 	
+	//get nearby places from foursquare
 	getFoursquareData(callback);
 }
 
